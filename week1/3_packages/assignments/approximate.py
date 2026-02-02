6c65a6eaaef846727b41c4072df2590563891275
Bas Terwijn <bterwijn@gmail.com>
2026-02-02 15:44:00 +0100

    update

---
+++ week1/3_packages/assignments/approximate.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f1(x):
+    return -0.0001242*x**5 + 0.00181*x**4 - 0.006097*x**3 - 0.3235*x**2 - 0.1989*x + 2.488
+
+def f2(x):
+    return 0.00007053*x**6 + 0.00003856*x**5 - 0.01555*x**4 - 0.02159*x**3 + 0.7697*x**2 + 1.206*x - 5.734
+
+def get_intersection_x_coords():
+    intersection_x = []
+    return intersection_x
+
+print(get_intersection_x_coords())
