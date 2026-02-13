6af9d5e58ec1b64fc106367505e174fd09dfa344
Bas Terwijn <bterwijn@gmail.com>
2026-02-13 20:34:32 +0100

    setting 10MB warning on zip file creation, instead of 5MB

--- zip_linux.sh
+++ zip_linux.sh
@@ -22,8 +22,8 @@ find "$WEEK" -type d -name assignments -exec \
 ## If this fails use:
 # zip -r $ZIP_FILE ./$WEEK
 
-if [ -f "$ZIP_FILE" ] && [ "$(stat -c %s -- "$ZIP_FILE")" -gt 5000000 ]; then
-  echo "The '$ZIP_FILE' is larger than 5MB, see if you can remove some files from '$WEEK' that are not needed for grading and remake this file."
+if [ -f "$ZIP_FILE" ] && [ "$(stat -c %s -- "$ZIP_FILE")" -gt 10000000 ]; then
+  echo "The '$ZIP_FILE' is larger than 10MB, see if you can remove some files from '$WEEK' that are not needed for grading and remake this file."
 fi
 
 if [ -f "$ZIP_FILE" ]; then
--- zip_mac.sh
+++ zip_mac.sh
@@ -22,8 +22,8 @@ find "$WEEK" -type d -name assignments -exec \
 # zip -r "$ZIP_FILE" "./$WEEK" -x "*.DS_Store"
 
 # macOS stat: -f %z prints file size in bytes
-if [ -f "$ZIP_FILE" ] && [ "$(stat -f %z -- "$ZIP_FILE")" -gt 5000000 ]; then
-  echo "The '$ZIP_FILE' is larger than 5MB, see if you can remove some files from '$WEEK' that are not needed for grading and remake this file."
+if [ -f "$ZIP_FILE" ] && [ "$(stat -f %z -- "$ZIP_FILE")" -gt 10000000 ]; then
+  echo "The '$ZIP_FILE' is larger than 10MB, see if you can remove some files from '$WEEK' that are not needed for grading and remake this file."
 fi
 
 if [ -f "$ZIP_FILE" ]; then
