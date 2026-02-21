dc6c44e0ce4f609c8b7aed22442aa8d07bed73a4
Bas Terwijn <bterwijn@gmail.com>
2026-02-21 18:06:41 +0100

    update

--- week3/run_all_tests.sh
+++ week3/run_all_tests.sh
@@ -3,4 +3,4 @@ cd ./1_object_oriented/assignments ; pytest -q --tb=no test_alarm_clock.py ; cd 
 cd ./1_object_oriented/assignments ; pytest -q --tb=no test_event.py ; cd -
 cd ./1_object_oriented/assignments ; pytest -q --tb=no test_time.py ; cd -
 cd ./2_polymorphism/assignments ; pytest -q --tb=no test_game_composition.py ; cd -
-cd ./3_lazy_evaluation/assignment ; pytest -q --tb=no test_lazy.py ; cd -
+cd ./3_lazy_evaluation/assignments ; pytest -q --tb=no test_lazy.py ; cd -
